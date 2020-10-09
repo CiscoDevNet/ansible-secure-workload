263dc7c871134a3b4869a6d8e5c4e50a03fec813
Joe Jacobs <joej@ignw.io>
2020-10-08 20:28:31 -0700

    fixing files and flake 8 tests that are failing

--- library/tetration_software_agent_config_intent.py
+++ library/tetration_software_agent_config_intent.py
@@ -1,16 +1,9 @@
-#!/usr/bin/python
-# Copyright (c) 2018 Red Hat, Inc.
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-from __future__ import absolute_import, division, print_function
-__metaclass__ = type
-
-
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
 
-DOCUMENTATION = r'''
+DOCUMENTATION = '''
 ---
 author: Brandon Beck (@techbeck03)
 description: Enables creation, modification, deletion and query of software agent
@@ -61,7 +54,7 @@ requirements: tetpyclient
 version_added: '2.8'
 '''
 
-EXAMPLES = r'''
+EXAMPLES = '''
 # Add or Modify agent config intent
 tetration_software_agent_config_intent:
     tenant_name: ACME
@@ -97,7 +90,7 @@ tetration_software_agent_config_intent:
       api_secret: 1234567890QWERTY
 '''
 
-RETURN = r'''
+RETURN = '''
 ---
 object:
   contains:
@@ -137,12 +130,12 @@ object:
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.six import iteritems, iterkeys
-from ansible.module_utils.tetration.api import TetrationApiModule
-from ansible.module_utils.tetration.api import TETRATION_API_SCOPES
-from ansible.module_utils.tetration.api import TETRATION_API_AGENT_CONFIG_PROFILES
-from ansible.module_utils.tetration.api import TETRATION_API_AGENT_CONFIG_INTENTS
-from ansible.module_utils.tetration.api import TETRATION_API_INVENTORY_FILTER
+from ansible.module_utils.tetration import TetrationApiModule
+from ansible.module_utils.tetration_constants import TETRATION_API_SCOPES
+from ansible.module_utils.tetration_constants import TETRATION_API_AGENT_CONFIG_PROFILES
+from ansible.module_utils.tetration_constants import TETRATION_API_AGENT_CONFIG_INTENTS
+from ansible.module_utils.tetration_constants import TETRATION_API_INVENTORY_FILTER
+
 
 def main():
     ''' Main entry point for module execution
@@ -188,7 +181,7 @@ def main():
         changed=False,
         object=None,
     )
-    
+
     state = module.params['state']
     check_mode = module.check_mode
     tenant_name = module.params['tenant_name']
@@ -206,13 +199,13 @@ def main():
     # Get current state of the object
     if root_app_scope_id:
         existing_app_scope = tet_module.run_method(
-            method_name = 'get',
-            target = '%s/%s' % (TETRATION_API_SCOPES, root_app_scope_id)
+            method_name='get',
+            target='%s/%s' % (TETRATION_API_SCOPES, root_app_scope_id)
         )
     elif not root_app_scope_id:
         existing_app_scope = tet_module.get_object(
-            target = TETRATION_API_SCOPES,
-            filter = dict(name=tenant_name)
+            target=TETRATION_API_SCOPES,
+            filter=dict(name=tenant_name)
         )
 
     if not existing_app_scope:
@@ -222,8 +215,8 @@ def main():
             module.fail_json(msg='Unable to find existing app scope named: %s' % tenant_name)
 
     existing_config_profile = tet_module.run_method(
-        method_name = 'get',
-        target = '%s/%s' % (TETRATION_API_AGENT_CONFIG_PROFILES, inventory_config_profile_id)
+        method_name='get',
+        target='%s/%s' % (TETRATION_API_AGENT_CONFIG_PROFILES, inventory_config_profile_id)
     )
 
     if not existing_config_profile:
@@ -231,23 +224,24 @@ def main():
 
     if inventory_filter_type == 'inventory':
         if inventory_filter_id:
-            existing_inventory_filter = run_method(
-                method_name = 'get',
-                target = '%s/%s' % (TETRATION_API_INVENTORY_FILTER, inventory_filter_id),
+            existing_inventory_filter = tet_module.run_method(
+                method_name='get',
+                target='%s/%s' % (TETRATION_API_INVENTORY_FILTER, inventory_filter_id),
             )
         else:
             app_scopes = tet_module.get_object(
-                target = TETRATION_API_SCOPES,
-                filter = dict(root_app_scope_id = existing_app_scope['root_app_scope_id']),
-                allow_multiple = True
+                target=TETRATION_API_SCOPES,
+                filter=dict(root_app_scope_id=existing_app_scope['root_app_scope_id']),
+                allow_multiple=True
             )
-            scope_ids = [ scope['id'] for scope in app_scopes ]
+            scope_ids = [scope['id'] for scope in app_scopes]
             inventory_filters = tet_module.run_method(
-                method_name = 'get',
-                target = TETRATION_API_INVENTORY_FILTER,
+                method_name='get',
+                target=TETRATION_API_INVENTORY_FILTER,
             )
             if inventory_filters:
-                inventory_filters = [ valid_filter for valid_filter in inventory_filters if valid_filter['app_scope_id'] in scope_ids and valid_filter['name'] == inventory_filter_name ]
+                inventory_filters = [valid_filter for valid_filter in inventory_filters if valid_filter['app_scope_id']
+                                     in scope_ids and valid_filter['name'] == inventory_filter_name]
                 existing_inventory_filter = inventory_filters[0] if len(inventory_filters) == 1 else None
             else:
                 existing_inventory_filter = None
@@ -259,16 +253,16 @@ def main():
 
     elif inventory_filter_type == 'scope':
         if inventory_filter_id:
-            existing_inventory_filter = run_method(
-                method_name = 'get',
-                target = '%s/%s' % (TETRATION_API_SCOPES, inventory_filter_id),
+            existing_inventory_filter = tet_module.run_method(
+                method_name='get',
+                target='%s/%s' % (TETRATION_API_SCOPES, inventory_filter_id),
             )
         else:
             existing_inventory_filter = tet_module.get_object(
-                target = TETRATION_API_SCOPES,
-                filter = dict(
-                    root_app_scope_id = existing_app_scope['root_app_scope_id'],
-                    name = inventory_filter_name
+                target=TETRATION_API_SCOPES,
+                filter=dict(
+                    root_app_scope_id=existing_app_scope['root_app_scope_id'],
+                    name=inventory_filter_name
                 )
             )
         if not existing_inventory_filter:
@@ -278,10 +272,10 @@ def main():
                 module.fail_json(msg='Unable to find scope named: %s' % inventory_filter_name)
 
     existing_config_intent = tet_module.get_object(
-        target = TETRATION_API_AGENT_CONFIG_INTENTS,
-        filter = dict(
-            inventory_filter_id = existing_inventory_filter['id'],
-            inventory_config_profile_id = inventory_config_profile_id
+        target=TETRATION_API_AGENT_CONFIG_INTENTS,
+        filter=dict(
+            inventory_filter_id=existing_inventory_filter['id'],
+            inventory_config_profile_id=inventory_config_profile_id
         )
     )
 
@@ -290,15 +284,15 @@ def main():
     # ---------------------------------
     if state == 'present':
         new_object = dict(
-            inventory_filter_id = existing_inventory_filter['id'],
-            inventory_config_profile_id = inventory_config_profile_id
+            inventory_filter_id=existing_inventory_filter['id'],
+            inventory_config_profile_id=inventory_config_profile_id
         )
         if not existing_config_intent:
             if not check_mode:
                 tet_module.run_method(
-                    method_name = 'post',
-                    target = TETRATION_API_AGENT_CONFIG_INTENTS,
-                    req_payload = new_object
+                    method_name='post',
+                    target=TETRATION_API_AGENT_CONFIG_INTENTS,
+                    req_payload=new_object
                 )
             result['object'] = new_object
             result['changed'] = True
@@ -312,8 +306,8 @@ def main():
         if existing_config_intent:
             if not check_mode:
                 tet_module.run_method(
-                    method_name = 'delete',
-                    target = '%s/%s' % (TETRATION_API_AGENT_CONFIG_INTENTS, existing_config_intent['id'])
+                    method_name='delete',
+                    target='%s/%s' % (TETRATION_API_AGENT_CONFIG_INTENTS, existing_config_intent['id'])
                 )
             result['changed'] = True
     # ---------------------------------
@@ -321,7 +315,7 @@ def main():
     # ---------------------------------
     else:
         result['object'] = existing_config_intent
-    
+
     module.exit_json(**result)
 
 
